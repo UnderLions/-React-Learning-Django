6ab1eaf4ae8e679080d6d4b00a4cb7d042f7db16
VidyaYadavv1519 <vidyayadav617@gmail.com>
2024-01-21 11:26:24 +0530

    Add API Endpoint for Blog Post Creation

--- leadmanager/blog/views.py
+++ leadmanager/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
+class BlogPostCreateView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = BlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
